75ffa401babf066ccc0da2a65072d2b597277ff1
mayaschrum <60490182+mayaschrum@users.noreply.github.com>
2020-02-12 15:40:08 -0800

    Add files via upload

---
+++ 02-03 taijitu classwork/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+   background(64, 0, 255)
+  fill(0)
+  stroke (16, 250, 12)
+  line(0,0,400,400)
+  line(0,400,400,0)
+  fill (250, 12, 238,)
+  strokeWeight (20)
+  stroke (255, 255, 0)
+  ellipse(200,200,200,200)
+}
+
+function draw() {
+}
