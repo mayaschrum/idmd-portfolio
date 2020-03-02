8397eb2ce03f24cac70793a6837f412ddad7dc09
mayaschrum <60490182+mayaschrum@users.noreply.github.com>
2020-03-02 11:34:24 -0800

    Add files via upload

---
+++ 02-24 font example classwork/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 300);
+  background (165, 242, 209)
+  textSize(20);
+  textFont("Yeon Sung");
+}
+function draw() {
+  text("Hello my name is Maya!!", 20, height);
+}
